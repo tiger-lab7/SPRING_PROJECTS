58d3989b8f611a62267c9296b3c962d6666c8bd7
Igor <igorletterbox@gmail.com>
2023-06-25 23:27:30 +0500

    Next

--- JERSEY_GLASSFISH/src/main/java/com/spring/JERSEY_GLASSFISH/JerseyConfig.java
+++ JERSEY_GLASSFISH/src/main/java/com/spring/JERSEY_GLASSFISH/JerseyConfig.java
@@ -3,6 +3,8 @@ package com.spring.JERSEY_GLASSFISH;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
+// В отличие от обычного Spring Boot, Jersey требует подобной регистрации контроллеров в классе ResourceConfig
+
 @Configuration
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
